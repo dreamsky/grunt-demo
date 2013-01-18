9c15d144018241dd66615f8d91cd0b08ff3d5ca7
Hongbo Li <lhb25@163.com>
2013-01-18 17:57:54 +0800

    update

---
+++ src/GruntDemo.js
@@ -0,0 +1,28 @@
+/*
+ * GruntDemo
+ * https://github.com/dreamsky/grunt-demo
+ *
+ * Copyright (c) 2013 Hongbo Li
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
